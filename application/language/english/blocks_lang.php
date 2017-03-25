bff60e93a640af3be7a3f6198589713192d3665f
srammram-subhash <bose@srammram.com>
2017-03-25 13:27:22 +0530

    Added the sql file

--- application/language/english/blocks_lang.php
+++ application/language/english/blocks_lang.php
@@ -1,4 +1,10 @@
 <?php
+$lang['blocks_module_label'] = "Block";
+$lang['block_types']       = "Block Types";
+$lang['type']       = "Type";
+$lang['order']       = "Order";
+$lang['page']      = "Page";
+$lang['position']  = "Position";
 $lang['block_element_id'] = "Block Id";
 $lang['block_element_class'] = "Block Class";
 
