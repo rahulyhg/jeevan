6c7e7ff8b91a97959d3497cbf6270b2bb21f0080
ananthan1991 <ananthan@srammram.com>
2017-03-27 11:40:20 +0530

    jeevan home pages

---
+++ application/modules/adminpanel/views/blocks/blocks/edit-latestupdate.php
@@ -0,0 +1,8 @@
+<?php ?>
+<div class="form-group">
+	<label for="inputEmail3" class="col-sm-2 control-label"><?php echo 'Latest Update'.get_required();?></label>
+	<div class="col-sm-<?php echo get_form_size();?>"> <div class="input_box">
+	 <?php echo form_dropdown("params[latestupdate_view]",$type_params['latestupdate_view'],$records["type_params"]["latestupdate_view"], "class=\"form-control\""); ?>						 
+		</div>
+	</div>
+</div>
---
+++ application/modules/adminpanel/views/blocks/blocks/edit-newsletter.php
@@ -0,0 +1,40 @@
+<?php ?>
+<div class="form-group">
+	<label for="inputEmail3" class="col-sm-2 control-label"><?php echo 'Type'.get_required();?></label>
+	<div class="col-sm-<?php echo get_form_size();?>"> <div class="input_box">
+	 <?php echo form_dropdown("params[newsletter_view]",$type_params['newsletter_view'],$records["type_params"]["newsletter_view"], "class=\"form-control required\""); ?>						 
+		</div>
+	</div>
+</div>
+
+<div class="form-group">
+	<label for="inputEmail3" class="col-sm-2 control-label"><?php echo 'Title'.get_required();?></label>
+    <div class="col-sm-<?php echo get_form_size();?>"> <div class="input_box">
+    	<input type="text" name="params[newsletter_title]" value="<?php echo $records["type_params"]["newsletter_title"]; ?>" class="form-control required">
+    </div></div>
+</div>
+<div class="form-group">
+	<label for="inputEmail3" class="col-sm-2 control-label"><?php echo 'Sub Title';?></label>
+    <div class="col-sm-<?php echo get_form_size();?>"> <div class="input_box">
+    	<input type="text" name="params[newsletter_subtitle]" value="<?php echo $records["type_params"]["newsletter_subtitle"]; ?>" class="form-control">
+    </div></div>
+</div>
+<div class="form-group">
+	<label for="inputEmail3" class="col-sm-2 control-label"><?php echo 'Email Address';?></label>
+    <div class="col-sm-<?php echo get_form_size();?>"> <div class="input_box">
+    	<input type="text" name="params[newsletter_email]" value="<?php echo $records["type_params"]["newsletter_email"]; ?>" class="form-control">
+    </div></div>
+</div>
+<div class="form-group">
+	<label for="inputEmail3" class="col-sm-2 control-label"><?php echo 'Icon'.get_required();?></label>
+	<div class="col-sm-<?php echo get_form_size();?>"> <div class="input_box">
+					 <?php echo form_dropdown("params[newsletter_icon]",$type_params['newsletter_icon'],$records["type_params"]["newsletter_icon"], "class=\"form-control required\""); ?>	
+		</div>
+	</div>
+</div>
+<div class="form-group">
+	<label for="inputEmail3" class="col-sm-2 control-label"><?php echo 'Button Name'; ?></label>
+    <div class="col-sm-<?php echo get_form_size();?>"> <div class="input_box">
+    	<input type="text" name="params[newsletter_button]" value="<?php echo $records["type_params"]["newsletter_button"]; ?>" class="form-control">
+    </div></div>
+</div>
