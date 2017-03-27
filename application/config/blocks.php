c1d694ef3def6c7d6d9535977282131c60537234
ananthan1991 <ananthan@srammram.com>
2017-03-27 11:09:32 +0530

    jeevan home pages

--- application/config/blocks.php
+++ application/config/blocks.php
@@ -7,16 +7,23 @@ $block_postions = array(
 	'site_header' => 'Site Header',
 	'site_footer' => 'Site Footer',
 	'site_leftmenu' => 'Site Left Menu',
-	'news_scroll' => 'News Scroll'
+	'news_scroll' => 'News Scroll',
+	'latestupdate' => 'Latest Update',
+	'menus' => 'Menus',
+	'site_latest' => 'Site Latest Menus',
+	'site_header_desktop' => 'site_header_desktop',
+	'site_copyright' => 'Site Copyright',
+	'way_circle' => 'Way Of life circle',
+	'content_newsletter' => 'Content Newsletter'
 	
 );
 $pages['all'] = array(
 	'title' => 'All',
-	'positions' => 'all_page_right,site_header,site_footer,site_leftmenu, content_middle'
+	'positions' => 'all_page_right,site_header, site_header_desktop, site_footer,site_leftmenu, site_copyright, content_middle, menus'
 );
 $pages['frontend/frontend/index'] = array(
 	'title' => 'Home',
-	'positions' => 'content_left, content_middle, content_right, content_middle'
+	'positions' => 'content_left, content_middle, way_circle, content_newsletter, site_latest'
 );
 
 
@@ -24,7 +31,9 @@ $pages['frontend/frontend/index'] = array(
 
 $block_types = array(
 	"textwidgets" => "Text Widget",
+	"latestupdate" => "Latest Update",
 	"slidernews" => "Slider News",
 	"menus" => "Menus",
+	"wayoflife" => "Way Of Life",
 	"newsletter" => "News Letter",	
 );
