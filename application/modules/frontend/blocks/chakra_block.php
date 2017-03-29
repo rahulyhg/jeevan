08d67f48e5a8370866fd00e5f05c8e16baea190d
ananthan1991 <ananthan@srammram.com>
2017-03-29 10:17:09 +0530

    gallery modules

---
+++ application/modules/frontend/blocks/chakra_block.php
@@ -0,0 +1,12 @@
+<?php 
+class Chakra_Block extends Core_Block {
+	protected $view = 'blocks/chakra';	
+	public function __construct() {
+		parent::__construct();
+	}
+	public function drawData() {
+		$data = $this->getBlockData();
+		$data['accord_slug'] = $this->CI->uri->segment(4);
+		return $data;
+	}
+}
---
+++ application/modules/frontend/blocks/gallery_block.php
@@ -0,0 +1,15 @@
+<?php 
+class Gallery_Block extends Core_Block {
+	protected $view = 'blocks/gallery';	
+	public function __construct() {
+		parent::__construct();
+		
+	}
+	public function drawData() {
+		$data = $this->getBlockData();
+		$category_path = media_url();
+		$data['gallery'] = $this->CI->Mydb->get_all_records('id, name, slug, CONCAT("'.$category_path.'", category_image) AS category_image', 'sramcms_gallary_categories', array('is_active' => '1', 'is_delete' => '0'));
+		
+		return $data;
+	}
+}
