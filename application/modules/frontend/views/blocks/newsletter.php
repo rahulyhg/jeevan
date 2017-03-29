1a58e781438ef848f6491ebd6738bfa2f1dadcfb
ananthan1991 <ananthan@srammram.com>
2017-03-29 20:32:56 +0530

    newsletter module

--- application/modules/frontend/views/blocks/newsletter.php
+++ application/modules/frontend/views/blocks/newsletter.php
@@ -14,9 +14,14 @@ if(!empty($params)){
         </div>
         <?php } ?>
         </div>
-      <form id="newsletter_form" name="newsletter_form" action="newscontactform.php" method="post">
+      <form id="newsletter_form" name="newsletter_form" action="<?php echo frontend_url('newsletter'); ?>" method="post">
+      	<div class="form-group">
+        	<input type="text" name="newsletter_name"  id="newsletter_name" class="form-control" placeholder="Name">
+            <input type="hidden" name="action" value="Subcribe">
+        </div>
         <div class="input-group">
-            <input type="text" class="form-control" placeholder="E-mail" name="emailid" id="emailid">
+        	
+            <input type="text" class="form-control" placeholder="E-mail" name="newsletter_email" id="newsletter_email">
             <div class="input-group-btn">
                 <input class="btn btn-danger" id="news_submit" name="news_submit" value="<?php echo $params->newsletter_button; ?>" type="submit">
             </div>
@@ -24,6 +29,24 @@ if(!empty($params)){
     </form>
     <div class="contact_emailid"></div>
 </div>
+
+<script src="<?php echo skin_url(); ?>js/jquery.validate.js"></script>
+<script type="text/javascript">
+$(document).ready(function(e) {
+    
+
+		$("#newsletter_form").validate({
+		//ignore: ".ignore",
+		rules: {
+			emailid:{required:true, email: true},
+		},
+		messages: {
+			emailid:{required: "Enter your email id", email: "Invaild email id"},
+		},
+		
+	});
+});
+</script>
 <?php
 }
 ?>
