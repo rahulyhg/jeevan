774913c7f63f01d3456f208350e5e3d43aafb6c2
ananthan1991 <ananthan@srammram.com>
2017-03-29 20:32:20 +0530

    newsletter module

--- application/modules/frontend/controllers/Frontend.php
+++ application/modules/frontend/controllers/Frontend.php
@@ -19,6 +19,14 @@ class Frontend extends MY_Controller {
         $this->module_labels = "Home";
         $this->folder = "homepage/";
         $this->routeplan_table = "sramcms_routeplan";
+		$this->sramcms_newsletter_table = "sramcms_newsletter";
+		$this->load->library ( 'common' );
+		$config = Array(
+            'mailtype' => 'html',
+            'charset' => 'utf-8',
+            'wordwrap' => TRUE
+        );
+        $this->load->helper('emailtemplate');
     }
 
     /* this method used to show all dashboard all details... */
@@ -76,7 +84,100 @@ class Frontend extends MY_Controller {
 
         echo json_encode($response);
     }
+	
+	
+	public function newsletter(){
+		if($this->input->post('action') == 'Subcribe'){
+			
+			$this->form_validation->set_rules('newsletter_name', 'Name', 'required');
+			$this->form_validation->set_rules('newsletter_email', 'Email Address', 'required');
+			if ($this->form_validation->run($this) == TRUE) {
+				
+				$check = $this->Mydb->get_record('email', $this->sramcms_newsletter_table, array('is_active' => '1', 'is_delete' => '0', 'email' => $this->input->post('newsletter_email')));
+				
+				if(empty($check)){
+					$insert_array = array(
+						'name' => $this->input->post('newsletter_name'),
+						'email' => $this->input->post('newsletter_email'),
+						'status' => '1',
+						'is_active' => '1',
+						'is_delete' => '0',
+						'created' => current_date(),
+						'activation_code' => md5(uniqid(rand()))
+					);
+					$insert = $this->Mydb->insert($this->sramcms_newsletter_table, $insert_array);
+					
+					$details = $this->Mydb->get_record('name, email, activation_code', $this->sramcms_newsletter_table, array('id' => $insert));
 
+
+					$name = $details['name'];
+					$activation_link = frontend_url('newsletterunsubscribe/'.$details['activation_code']);
+					$to_email = $details['email'];
+					$response = $this->send_newletter_email($name, $to_email, $activation_link);
+				
+					if($insert){
+						$response ['status'] = 'status';
+                		$response ['message'] = 'Your email is subscribe successfully';
+					}else{
+						$response ['status'] = 'error';
+                		$response ['message'] = 'Your email is not subscribe';
+					}
+					
+				}else{
+					$response ['status'] = 'error';
+                	$response ['message'] = 'Your email already exits. please change email';
+				}	
+							
+			}else{
+				$response ['status'] = 'error';
+                $response ['message'] = validation_errors();
+			}
+			echo json_encode($response);
+			exit();
+			
+		}
+	}
+	
+	public function newsletterunsubscribe(){
+		
+		$data = array();
+        $data['module_label'] = $this->module_label;
+        $data['module_labels'] = $this->module_label;
+        $data['module'] = $this->module;
+        $this->loadBlocks();
+        $data = array_merge($data, $this->view_data);
+		
+		$key = $this->uri->segment(3);
+		if (!empty($key)) {
+            $getsubscribe = $this->Mydb->custom_query("SELECT id FROM $this->sramcms_newsletter_table WHERE activation_code='$key'");
+            if (!empty($getuserid)) {
+
+                $id = $getsubscribe[0]['id'];
+                $user_array = array(
+                    'status' => 0,
+					'created' => date()
+                );
+
+                $updateuser = $this->Mydb->update(activation_code, array('id' => $id), $user_array);
+                $data['emailmsg'] = "Your Unsubscribe code is success";
+            } else {
+                $data['emailmsg'] = "Your Unsubscribe code is wrong . Please try again";
+            }
+        } else {
+            $data['emailmsg'] = "Something Error. Please try again";
+        }
+		
+        $this->layout->display_frontend($this->folder . '/homepage', $data);
+	}
+	
+	public function send_newletter_email($name, $to_email, $activation_link) {
+
+        $chk_arr = array('[NAME]', '[ACTIVATIONLINK]');
+        $rep_arr = array($name, $activation_link);
+        $response = send_email($to_email, $template_slug = "newsletter-subscribe", $chk_arr, $rep_arr, $attach_file = array(), $path = '', $subject = '', $cc = '', $html_template = 'email_template');
+        return $response;
+    }
+	
     public function logout() {
 
         $this->session->sess_destroy();
