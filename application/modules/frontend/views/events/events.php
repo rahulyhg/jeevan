753124666fa94af383868f0566040a82ac1f5102
srammram-subhash <bose@srammram.com>
2017-03-29 11:13:41 +0530

    Added the event module

---
+++ application/modules/frontend/views/events/events.php
@@ -0,0 +1,52 @@
+
+<div class="container" style="margin-top:150px;">
+<h1>Event Calendar</h1>
+	<div class="page-header">
+
+		<div class="pull-right form-inline">  
+			<div class="btn-group">
+				<button class="btn btn-primary" data-calendar-nav="prev"><< Prev</button>
+				<button class="btn btn-default" data-calendar-nav="today">Today</button>
+				<button class="btn btn-primary" data-calendar-nav="next">Next >></button>
+			</div>
+			<div class="btn-group">
+				<button class="btn btn-warning" data-calendar-view="year">Year</button>
+				<button class="btn btn-warning active" data-calendar-view="month">Month</button>
+				<button class="btn btn-warning" data-calendar-view="week">Week</button>
+				<button class="btn btn-warning" data-calendar-view="day">Day</button>
+			</div>
+		</div>
+
+		<h3></h3>
+		
+	</div>
+
+	<div class="row">
+		<div class="col-md-12">
+			<div id="calendar"></div>
+		</div>
+		
+	</div>
+
+	<div class="clearfix"></div>
+	<br><br>
+	<div id="disqus_thread"></div>
+	
+	<div class="modal fade" id="events-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+					<h3 class="modal-title">Event</h3>
+				</div>
+				<div class="modal-body" style="height: 400px">
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	
+	</div>
