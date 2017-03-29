db93186c7c8d194e968c24b81dfc6046f0369c31
ananthan1991 <ananthan@srammram.com>
2017-03-29 10:14:48 +0530

    gallery modules

---
+++ application/config/blocks/gallery.php
@@ -0,0 +1,8 @@
+<?php
+$type_params['gallery_view'] = array(
+	' ' => 'Select Gallery View',
+	'gallery_slider_one' => 'Gallery Slider One',
+	'gallery_slider_two' => 'Gallery Slider Two',
+	'gallery_slider_three' => 'Gallery Slider Three',
+	'gallery_slider_four' => 'Gallery Slider Four'
+);
