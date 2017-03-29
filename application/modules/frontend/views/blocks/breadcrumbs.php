c76addbe3b4717c72dd282b395f2c4378508f8e6
ananthan1991 <ananthan@srammram.com>
2017-03-29 18:19:23 +0530

    gallery filter

--- application/modules/frontend/views/blocks/breadcrumbs.php
+++ application/modules/frontend/views/blocks/breadcrumbs.php
@@ -5,7 +5,7 @@ if(!empty($page_breadcrums)){
 	<div class="container">
 		<div class="row">
 			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-				<h5><a href="index.php">Home</a> <span class="coloum">|</span> <span><?php echo $page_breadcrums; ?></span></h5>
+				<h5><a href="<?php echo BASE_URL(); ?>">Home</a> <span class="coloum">|</span> <span><?php echo $page_breadcrums; ?></span></h5>
 			</div>
 		</div>
 	</div>
