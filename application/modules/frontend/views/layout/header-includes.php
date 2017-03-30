b0628a3396b53f946e90e4c63ec57ef21fca9263
ananthan1991 <ananthan@srammram.com>
2017-03-30 11:22:20 +0530

    newsletter module

--- application/modules/frontend/views/layout/header-includes.php
+++ application/modules/frontend/views/layout/header-includes.php
@@ -9,21 +9,13 @@
 <link rel="stylesheet" href="<?php echo skin_url(); ?>OCRAStd/styles.css">
 <link rel="stylesheet" href="<?php echo skin_url(); ?>font-awesome/css/font-awesome.min.css">
 <link rel="stylesheet" href="<?php echo skin_url(); ?>css/stylesheet.css">
-<link rel="stylesheet" href="<?php echo skin_url(); ?>css/w3.css">
 <link rel="stylesheet" href="<?php echo skin_url(); ?>lato_fonts/stylesheet.css">
-<link rel="stylesheet" href="<?php echo skin_url(); ?>css/owl.carousel.css">
-<link rel="stylesheet" href="<?php echo skin_url(); ?>css/owl.theme.default.min.css">
-<link rel="stylesheet" href="<?php echo skin_url(); ?>css/jquery.bxslider.css">
-<link rel="stylesheet" href="<?php echo skin_url(); ?>css/pgwslideshow.min.css">
 <link rel="stylesheet" href="<?php echo skin_url(); ?>css/bootstrap-datetimepicker.css">  
 <link rel="stylesheet" href="<?php echo skin_url(); ?>event/css/calendar.css">  
 
 <script src="<?php echo skin_url(); ?>js/jquery-3.1.1.min.js"></script>
 <script src="<?php echo skin_url(); ?>js/bootstrap.min.js"></script>
 <script src="<?php echo skin_url(); ?>js/marquee.js"></script>
-<script src="<?php echo skin_url(); ?>js/owl.carousel.min.js"></script>
-<script src="<?php echo skin_url(); ?>js/jquery.bxslider.min.js"></script>
-<script src="<?php echo skin_url(); ?>js/pgwslideshow.min.js"></script>
 <script src="<?php echo skin_url(); ?>js/jquery.validate.js"></script>
 <script src="<?php echo skin_url(); ?>js/bootstrap-datetimepicker.js"></script>
 <?php /* common javascript varibles ...*/ ?>
