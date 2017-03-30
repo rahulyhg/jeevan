8fa92c99315961e046047fdd89d0adde4540fda7
srammram-subhash <bose@srammram.com>
2017-03-30 19:01:54 +0530

    Added the event users list

---
+++ application/modules/adminpanel/controllers/Eventusers.php
@@ -0,0 +1,398 @@
+<?php
+/**************************
+Project Name	: POS
+Created on		: 19 Feb, 2016
+Last Modified 	: 19 Feb, 2016
+Description		: Page contains dashboard related functions.
+
+***************************/
+defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' );
+class Eventusers extends CI_Controller {
+	public function __construct() {
+		parent::__construct ();
+		
+		$this->authentication->admin_authentication();
+		$this->module = "eventusers";
+		$this->module_label = "Appointment Users";
+		$this->module_labels = "Appointment Users";
+		$this->folder = "eventusers/";
+		$this->table = "sramcms_event_users";
+		$this->event_table = "sramcms_routeplan";
+		$this->primary_key = 'id';
+		$this->load->library('common');
+		
+			
+	}
+	function _remap($method, $args) {
+		
+		if (method_exists($this, $method)) {
+			
+				if($method == "ajax_pagination"){
+					$page = isset($args[0]) ? $args[0] :'';
+					$this->$method($page);
+				}else{
+					$this->$method($args);
+				}	
+			
+		} else {			
+				$this->index($method, $args);
+			
+		}
+	}
+	/* this method used to show all dashboard all details... */
+	public function index($method, $args = array()) {
+		
+	  $data = array();
+	  
+	  $data['module_label'] = $this->module_label;
+	  $data['module_labels'] = $this->module_label;
+	  $data['module'] = $this->module;
+	  if (!empty($method) && empty($args[0])) {
+	  	$event_id = decode_value($method);
+	  	$this->session->set_userdata ( $this->module . "_event_id", $event_id );
+	  }
+	
+	  $this->layout->display_admin($this->folder.$this->module."-list" ,$data);
+	}
+	
+	public function add() {
+			
+		$data = $this->load_module_info();
+		if ($this->input->post( 'action' ) == "Add") {
+			check_ajax_request ();	
+			$this->form_validation->set_rules ( 'name', 'lang:name', 'trim|required' );
+			$this->form_validation->set_rules ( 'email', 'lang:email', 'trim|required' );
+			$this->form_validation->set_rules ( 'phone_no', 'lang:phone_no', 'trim|required' ); 
+			$this->form_validation->set_rules ( 'purpose_of_appointment[]', 'lang:purpose_of_appointment', 'trim|required' );
+			$this->form_validation->set_rules ( 'booked_date', 'lang:booked_date', 'trim|required' );
+			
+			if ($this->form_validation->run () == TRUE) {				
+				
+				$event_id = get_session_value ( $this->module . '_event_id' );		
+				
+				$check_exist = $this->Mydb->get_record('*', $this->table, array('event_id' => $event_id, 'email' => $this->input->post('email'),'booked_date' => $this->input->post('booked_date')));
+				
+				if(empty($check_exist)){
+					$purpose_of_appoint = json_encode($this->input->post('purpose_of_appointment'));				
+					$insert_data = array(
+							"name" => $this->input->post('name'),
+							"email" => $this->input->post('email'),
+							"phone_no" => $this->input->post('phone_no'),
+							"event_id" => $event_id,
+							"booked_date" => $this->input->post('booked_date'),
+							"purpose_of_appointment" => $purpose_of_appoint ? $purpose_of_appoint :'',
+							"message" => $this->input->post('message'),
+							"created_on" => current_date(),
+							"created_ip" => get_ip(),
+							"created_by" => get_admin_id(),
+							"is_active" => '1',
+					);
+					$this->Mydb->insert($this->table ,$insert_data);		
+					
+					$msg = "A New ".$this->module_label.'has been added';
+					create_log('new',$this->module_label,$msg);
+					$this->session->set_flashdata ( 'action_success', sprintf ( $this->lang->line ( 'success_message_add' ), $this->module_label ) );
+					$result ['status'] = 'success';
+					
+				}else{
+					
+					$result ['status'] = 'error';
+					$result ['message'] = "The Appointment Already Booked using same email";
+				}
+			}
+			else {
+				$result ['status'] = 'error';
+				$result ['message'] = validation_errors ();
+			}	
+			echo json_encode ( $result );
+			exit ();
+		}			
+		
+		/* Common labels */
+		$data ['breadcrumb'] = $data ['form_heading'] = get_label ( 'add' ) . ' ' . $this->module_label;
+		$data ['module_action'] = 'add' ;
+		$data ['event_data'] = $this->Mydb->get_record ( '*', $this->event_table, array (
+				$this->primary_key => get_session_value ( $this->module . '_event_id' )
+		) );
+		$this->layout->display_admin ( $this->folder . $this->module . "-add", $data );
+	}
+	
+	/* this method used list ajax listing... */
+	function ajax_pagination($page = 0) {
+		check_ajax_request (); /* skip direct access */
+		$data = $this->load_module_info ();
+		$where = " WHERE is_delete != '1' ";
+		$like = "";
+		if (post_value ( 'paging' ) == "") {
+			$this->session->set_userdata ( $this->module . "_search_field", post_value ( 'search_field' ) );
+			$this->session->set_userdata ( $this->module . "_search_value", post_value ( 'search_value' ) );
+			$this->session->set_userdata ( $this->module . "_search_status",post_value ( 'status' ));
+		}
+		if (get_session_value ( $this->module . "_event_id" ) != "") {
+			$where .= 'AND event_id = "'.get_session_value ( $this->module . '_event_id' ).'"';
+		}
+		
+		if (get_session_value ( $this->module . "_search_status" ) != "") {
+			$where .= 'AND is_active = "'.get_session_value ( $this->module . '_search_status' ).'"';	
+		}
+		
+		
+		if (get_session_value ( $this->module . "_search_field" ) != "" && get_session_value ( $this->module . "_search_value" ) != "") {
+			$like .= ' AND '.get_session_value ( $this->module . "_search_field" ).' LIKE "%'.get_session_value ( $this->module . '_search_value' ) .'%"';
+		}
+		$filter = $where.$like;
+		$get_rows = $this->Mydb->custom_query("SELECT * FROM $this->table $where $like ORDER BY id" );
+		
+		/**
+		 * * pagination part start **
+		 */
+		 
+		$total_rows = count($get_rows);
+		$admin_records = 100;
+		$limit = (( int ) $admin_records == 0) ? 25 : $admin_records;
+		$offset = (( int ) $page == 0) ? 0 : $page; // ((int)$this->input->post('page') == 0 )? 0 : ($this->input->post('page') -1) * $limit;(int)$this->uri->segment(4);
+		$uri_segment = $this->uri->total_segments ();
+		$uri_string = admin_url () . $this->module . "/ajax_pagination";
+		$config = pagination_config ( $uri_string, $total_rows, $limit, $uri_segment );
+		$this->pagination->initialize ( $config );
+		$data ['paging'] = $this->pagination->create_links ();
+		
+		$data ['per_page'] = $data ['limit'] = $limit;
+		$data ['start'] = $offset;
+		$data ['total_rows'] = $total_rows;
+		/**
+		 * * pagination part end **
+		 */
+		
+		$data ['records'] = $this->Mydb->custom_query("SELECT * FROM $this->table $where $like ORDER BY id LIMIT $offset, $limit" );
+		
+			
+		$active_page = $offset = (( int ) $this->input->post ( 'page' ) == 0) ? 1 : $this->input->post ( 'page' );
+		
+		$html = get_template ( $this->folder . '/' . $this->module . '-ajax-list', $data );
+		echo json_encode ( array (
+				'status' => 'ok',
+				'sd' => post_value ( 'status' ),
+				'offset' => $offset,
+				'active_page' => $active_page,
+				'html' => $html 
+		) );
+		exit ();
+	}
+	
+	/* this method used to update record info.. */	
+	public function edit($edit_id = NULL) {
+		$data = $this->load_module_info ();
+		$id = addslashes ( decode_value ( $edit_id[0] ) );
+		$response = array ();
+		$record = $this->Mydb->get_record ( '*', $this->table, array (
+				$this->primary_key => $id
+		) );
+		(empty ( $record )) ? redirect ( admin_url() . $this->module ) : '';
+	
+		if ($this->input->post ( 'action' ) == "edit") {
+			check_ajax_request ();
+			
+			$this->form_validation->set_rules ( 'name', 'lang:name', 'trim|required' );
+			$this->form_validation->set_rules ( 'email', 'lang:email', 'trim|required' );
+			$this->form_validation->set_rules ( 'phone_no', 'lang:phone_no', 'trim|required' );
+			$this->form_validation->set_rules ( 'purpose_of_appointment[]', 'lang:purpose_of_appointment', 'trim|required' );
+			$this->form_validation->set_rules ( 'booked_date', 'lang:booked_date', 'trim|required' );
+			
+				
+			if ($this->form_validation->run () == TRUE) {
+				
+				$event_id = get_session_value ( $this->module . '_event_id' );
+				
+				$check_exist = $this->Mydb->get_record('*', $this->table, array('id !=' => $id,'event_id' => $event_id, 'email' => $this->input->post('email'),'booked_date' => $this->input->post('booked_date')));
+				
+				if(empty($check_exist)){
+					$purpose_of_appoint = json_encode($this->input->post('purpose_of_appointment'));
+					$update_data = array(
+							"name" => $this->input->post('name'),
+							"email" => $this->input->post('email'),
+							"phone_no" => $this->input->post('phone_no'),
+							"event_id" => $event_id,
+							"booked_date" => $this->input->post('booked_date'),
+							"purpose_of_appointment" => $purpose_of_appoint ? $purpose_of_appoint :'',
+							"message" => $this->input->post('message'),
+							"updated_on" => current_date(),
+							"updated_ip" => get_ip(),
+							"updated_by" => get_admin_id(),
+							"is_active" => $this->input->post('is_active'),
+					);
+					$this->Mydb->update ( $this->table, array ($this->primary_key => $record ['id'] ), $update_data );
+					
+					$msg = "A ".$this->module_label.' '.post_value('name').' has been edited';
+					create_log('edit',$this->module_label,$msg);
+					$this->session->set_flashdata ( 'action_success', sprintf ( $this->lang->line ( 'success_message_edit' ), $this->module_label ) );
+					$response ['status'] = 'success';
+					
+				}else{
+					
+					$response['status'] = 'error';
+					$response['message'] = "The Appointment Already Booked using same email";
+				}				
+				
+				
+			} else {
+				$response ['status'] = 'error';
+				$response ['message'] = validation_errors ();
+			}
+				
+			echo json_encode ( $response );
+			exit ();
+		}
+		$data ['records'] = $record;
+		/* Common labels */		
+		$data ['breadcrumb'] = $data ['form_heading'] = get_label ( 'edit' ) . ' ' . $this->module_label;
+		$data['edit_id'] = $edit_id;
+		$data ['event_data'] = $this->Mydb->get_record ( '*', $this->event_table, array (
+				$this->primary_key => get_session_value ( $this->module . '_event_id' )
+		) );
+		$data ['module_action'] = 'edit/' . encode_value ( $record [$this->primary_key] );
+		$this->layout->display_admin ( $this->folder . $this->module . '-edit', $data );
+	}
+	
+	/* this method used to common module labels */
+	private function load_module_info() {
+		$data = array ();
+		$data ['module_label'] = $this->module_label;
+		$data ['module_labels'] = $this->module_labels;
+		$data ['module'] = $this->module;
+		return $data;
+	}
+	
+	/* this method used update multible actions */
+	function action() {
+		$ids = ($this->input->post ( 'multiaction' ) == 'Yes' ? $this->input->post ( 'id' ) : decode_value ( $this->input->post ( 'changeId' ) ));
+	
+		$ids = ($this->input->post ( 'changeId' ) != '') ? decode_value ( $this->input->post ( 'changeId' ) ) : $this->input->post ( 'id' );
+		
+		$postaction = $this->input->post ( 'postaction' );
+		
+		$response = array (
+				'status' => 'error',
+				'msg' => get_label ( 'something_wrong' ),
+				'action' => '',
+				'multiaction' => $this->input->post ( 'multiaction' ) 
+		);
+		
+		if(is_array($ids)) {
+			$implode_ids = '('.implode(",",$ids).')';
+			$concat_name = $this->db->query("select GROUP_CONCAT(name) as name from $this->table where id in $implode_ids")->row_array();
+			$action_name = $concat_name['name'];
+		}
+		else {
+			$records = $this->Mydb->get_record('name',$this->table,array('id'=>$ids));
+			$action_name = $records['name'];
+		}
+		/* Delete */
+		if ($postaction == 'Delete' && ! empty ( $ids )) {
+			
+			// $this->Mydb->delete_where_in($this->table,'client_id',$ids,'');
+			$update_values = array (
+					"is_delete" => '1',
+					"updated_on" => current_date (),
+					'updated_by' => get_admin_id ()
+			);
+			
+			if (is_array ( $ids )) {
+				
+				$this->Mydb->update_where_in ( $this->table, $this->primary_key, $ids, $update_values );
+				
+				$response ['msg'] = sprintf ( $this->lang->line ( 'success_message_activate' ), $this->module_labels );
+			} else {
+					
+				$this->Mydb->update ( $this->table, array (
+						$this->primary_key => $ids 
+				), $update_values);
+				$response ['msg'] = sprintf ( $this->lang->line ( 'success_message_delete' ), $this->module_label );
+				
+				// $this->Mydb->delete($this->table,'client_id',$ids,'');
+			}
+			$msg = "A ".$this->module_label.' '.$action_name.' has been deleted ';
+			create_log('delete',$this->module_label,$msg);
+			$response ['status'] = 'success';
+			$response ['action'] = $postaction;
+		}
+		
+		/* Activation */
+		if ($postaction == 'Activate' && ! empty ( $ids )) {
+			$update_values = array (
+					"is_active" => '1',
+					"updated_on" => current_date (),
+			);
+			
+			if (is_array ( $ids )) {
+				$this->Mydb->update_where_in ( $this->table, $this->primary_key, $ids, $update_values );
+				$response ['msg'] = sprintf ( $this->lang->line ( 'success_message_activate' ), $this->module_labels );
+			} else {
+				$this->Mydb->update ( $this->table, array (
+						$this->primary_key => $ids 
+				), $update_values );
+				$response ['msg'] = sprintf ( $this->lang->line ( 'success_message_activate' ), $this->module_label );
+			}
+			$msg = "A ".$this->module_label.' '.$action_name.' has been Activated ';
+			create_log('status',$this->module_label,$msg);
+			$response ['status'] = 'success';
+			$response ['action'] = $postaction;
+		}
+		
+		/* Deactivation */
+		if ($postaction == 'Deactivate' && ! empty ( $ids )) {
+			$update_values = array (
+					"is_active" => '0',
+					"updated_on" => current_date (),
+					'updated_by' => get_admin_id ()
+			);
+			
+			if (is_array ( $ids )) {
+				
+				$this->Mydb->update_where_in ( $this->table, $this->primary_key, $ids, $update_values );
+				$response ['msg'] = sprintf ( $this->lang->line ( 'success_message_deactivate' ), $this->module_labels );
+			} else {
+				
+				$this->Mydb->update ( $this->table, array (
+						$this->primary_key => $ids 
+				), $update_values );
+				$response ['msg'] = sprintf ( $this->lang->line ( 'success_message_deactivate' ), $this->module_label );
+			}
+			$msg = "A ".$this->module_label.' '.$action_name.' has been Deactivated ';
+			create_log('status',$this->module_label,$msg);
+			$response ['status'] = 'success';
+			$response ['action'] = $postaction;
+		}
+		
+		echo json_encode ( $response );
+		exit ();
+	}
+	
+	
+	/* this method used to clear all session values and reset search values */
+	function refresh() {
+		$this->session->unset_userdata ( $this->module . "_search_field" );
+		$this->session->unset_userdata ( $this->module . "_search_value" );
+		$this->session->unset_userdata ( $this->module . "_search_category" );
+		$this->session->unset_userdata ( $this->module . "_search_status" );
+		redirect ( admin_url () . $this->module );
+	}
+	
+	public function category_check(){
+		$category_name = $this->input->post('category_name');
+		$categories_type = $this->input->post('category_type');
+		$record = $this->Mydb->custom_query("select * from categories where name = '".$category_name."'
+				   and parent_id = ".$categories_type." and is_delete != '1'");
+		if ($record)
+        {
+            $this->form_validation->set_message('category_name', "The %s field has an unique name.");
+            return FALSE;
+        }
+        else
+        {
+            return TRUE;
+        }
+		
+	}
+	
+}
