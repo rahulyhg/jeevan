73db5399aca0218c47948ad77fcafa52a1bd00a4
ananthan1991 <ananthan@srammram.com>
2017-03-30 21:15:55 +0530

    gallery video thumb add

---
+++ application/modules/frontend/views/homepage/subscribe.php
@@ -0,0 +1,16 @@
+<div class="aboutus_section">
+	<div class="container">
+		<div class="row">
+        	<?php
+			if(!empty($emailmsg)){
+			?>
+			<div class="col-xs-12 guru_aboutus text-center">
+				<h2>Subscribe</h2>
+				<p><?php echo $emailmsg; ?>.</p>
+			</div>
+			<?php
+			}
+			?>
+		</div>
+	</div>
+</div>
