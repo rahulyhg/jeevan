4d9bf4f7a4afa7f5bcd4cf1cfd3480af3045f5fa
ananthan1991 <ananthan@srammram.com>
2017-03-30 18:51:02 +0530

    newsletter module

--- application/modules/frontend/views/blocks/newsletter.php
+++ application/modules/frontend/views/blocks/newsletter.php
@@ -2,38 +2,41 @@
 if(!empty($params)){
 ?>
 
-<div class="col-lg-5 col-md-5 col-sm-5 col-xs-12 letter">
+<div class="col-lg-5 col-md-5 col-sm-5 col-xs-12 letter news_letter">
 
+
+<div class="col-lg-9 col-xs-12 letter_text">
     <h3><?php echo $params->newsletter_title; ?></h3>
-        <div class="row">
-        <div class="col-lg-10 col-xs-9 letter_text">
-            <p><?php echo $params->newsletter_subtitle; ?></p>
-        </div>
-        <?php if($params->newsletter_icon == 'show'){ ?>
-        <div class="col-lg-2 col-xs-3">
-            <img src="<?php echo skin_url(); ?>img/msg.png" alt="msg">
-        </div>
-        <?php } ?>
-        </div>
-        <form id="newsletter_form" class="form-horizontal" action="<?php echo frontend_url('newsletter'); ?>" name="newsletter_form" method="post">
-        
-      	<div class="form-group">
-        	<input type="text" name="newsletter_name"  id="newsletter_name" class="form-control" placeholder="Name">
-            <input type="hidden" name="action" value="Subcribe">
-        </div>
-        <div class="form-group">
-        	<input type="text" class="form-control" placeholder="E-mail" name="newsletter_email" id="newsletter_email">
-            <input type="hidden" name="action" value="Subcribe">
-        </div>
-        <div class="input-group">
-        	
-            
-            <div class="input-group-btn">
-                <input class="btn btn-danger" id="newsletter_submit" name="newsletter_submit" value="<?php echo $params->newsletter_button; ?>" type="submit">
+    <p><?php echo $params->newsletter_subtitle; ?></p>
+</div>
+
+<?php if($params->newsletter_icon == 'show'){ ?>
+<div class="col-lg-3 col-xs-3">
+	<img src="<?php echo skin_url(); ?>img/msg.png" alt="msg">
+</div>
+<?php } ?>
+
+
+    <div class="col-lg-12">
+        <form id="newsletter_form" class="form-horizontal" name="newsletter_form" method="post">
+            <div class="col-lg-5 col-xs-12 form-group">
+                <input type="text" name="newsletter_name"  id="newsletter_name" class="form-control" placeholder="Name">
+                <input type="hidden" name="action" value="Subcribe">
             </div>
-        </div>
-    </form>
-    <div class="newsletter_status"></div>
+            <div class="col-lg-5 col-xs-12 form-group">
+                <input type="text" class="form-control" placeholder="E-mail" name="newsletter_email" id="newsletter_email">
+            </div>
+            <div class="col-lg-2 col-xs-12 input-group">
+                <div class="btn-group">
+                    <input class="btn" id="newsletter_submit" name="newsletter_submit" value="<?php echo $params->newsletter_button; ?>" type="submit">
+                </div>
+            </div>
+        </form>
+        <div class="clearfix"></div><div class="newsletter_status"></div>
+    </div>
+
+
+  
 </div>
 
 <script type="text/javascript">
