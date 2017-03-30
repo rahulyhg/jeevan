f7be7ba7a983d2e3fe343bbb9b13c67a540459a1
ananthan1991 <ananthan@srammram.com>
2017-03-30 21:08:18 +0530

    gallery video thumb add

--- application/modules/frontend/blocks/gallery_block.php
+++ application/modules/frontend/blocks/gallery_block.php
@@ -8,6 +8,7 @@ class Gallery_Block extends Core_Block {
 	public function drawData() {
 		$data = $this->getBlockData();
 		$category_path = media_url();
+		
 		$data['gallery'] = $this->CI->Mydb->get_all_records('id, name, slug, CONCAT("'.$category_path.'", category_image) AS category_image', 'sramcms_gallary_categories', array('is_active' => '1', 'is_delete' => '0'));
 		
 		return $data;
