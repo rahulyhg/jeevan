efd3b75249125e9b75ca9ddc176146715b10374f
kalaivanan-sk <kannan.kalai58@gmail.com>
2017-04-06 14:48:47 +0530

    Modified Files Comitted

--- application/modules/frontend/views/pages/left-sidebar.php
+++ application/modules/frontend/views/pages/left-sidebar.php
@@ -14,8 +14,7 @@ if (!empty($blocks['inner_top'])) {
                 }
                 ?>
                 <?php
-                $address = $available_location;
-                $urlencode_address = urlencode($address);
+                $urlencode_address = urlencode($available_location);
                 ?>
                 <iframe width="100%" height="350" frameborder="0" scrolling="Yes" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en-&amp;geocode=&amp;abauth=52846723_CvfcNinFxxGfLf9q73NnFIgoyE&amp;authuser=0&amp;q=<?php echo $urlencode_address; ?>&amp;aq=0&amp;oq=svas&amp;vps=2&amp;jsv=469e&amp;sll=10.084754,78.77966&amp;sspn=0.002696,0.004699&amp;vpsrc=6&amp;num=10&amp;t=m&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe>
             </div>
