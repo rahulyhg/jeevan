08c1938fde0f14b0a3f36b9fb48401812c0ff075
kalaivanan-sk <kannan.kalai58@gmail.com>
2017-04-06 11:56:33 +0530

    Contact map  has been completed

--- application/modules/frontend/views/pages/left-sidebar.php
+++ application/modules/frontend/views/pages/left-sidebar.php
@@ -1,45 +1,52 @@
-<?php 
-if(!empty($blocks['inner_top'])){
-	echo $blocks['inner_top']; 
+<?php
+if (!empty($blocks['inner_top'])) {
+    echo $blocks['inner_top'];
 }
 ?>
 <div class="clearfix"></div>
 <div class="left-siderbar">
-	<div class="container">
-		<div class="row">
-        	<div class="col-lg-8">
-            	<?php
-				if(!empty($cms['page_description'])){
-				echo $cms['page_description'];
-				}
-				?>
-            </div>
-            <div class="col-lg-4">
-            	<?php 
-				if(!empty($blocks['inner_right'])){
-					echo $blocks['inner_right']; 
-				}
-				?>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8">
+                <?php
+                if (!empty($cms['page_description'])) {
+                    echo $cms['page_description'];
+                }
+                ?>
+                <?php
+                $address = $available_location;
+                $urlencode_address = urlencode($address);
+                ?>
+                <iframe width="100%" height="350" frameborder="0" scrolling="Yes" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=embed&amp;hl=en-&amp;geocode=&amp;abauth=52846723_CvfcNinFxxGfLf9q73NnFIgoyE&amp;authuser=0&amp;q=<?php echo $urlencode_address; ?>&amp;aq=0&amp;oq=svas&amp;vps=2&amp;jsv=469e&amp;sll=10.084754,78.77966&amp;sspn=0.002696,0.004699&amp;vpsrc=6&amp;num=10&amp;t=m&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe>
             </div>
         </div>
-   </div>
+    </div>
+    <div class="col-lg-4">
+        <?php
+        if (!empty($blocks['inner_right'])) {
+            echo $blocks['inner_right'];
+        }
+        ?>
+    </div>
+</div>
+</div>
 </div>
 <div class="clearfix"></div>
-<?php 
-if(!empty($blocks['inner_bottom'])){
-	echo $blocks['inner_bottom']; 
+<?php
+if (!empty($blocks['inner_bottom'])) {
+    echo $blocks['inner_bottom'];
 }
 ?>
 <div class="clearfix"></div>
 <div class="news_letter">
-	<div class="container">
-		<div class="row" >
-            <?php 
-			if(!empty($blocks['content_newsletter'])){
-			echo $blocks['content_newsletter']; 
-			}
-			?>
-            
-		</div>
-	</div>
+    <div class="container">
+        <div class="row" >
+            <?php
+            if (!empty($blocks['content_newsletter'])) {
+                echo $blocks['content_newsletter'];
+            }
+            ?>
+
+        </div>
+    </div>
 </div>
