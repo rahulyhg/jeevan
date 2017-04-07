d4bea66a2bd14bfa3a4c760ae3c4db1612a52f8d
ananthan1991 <ananthan@srammram.com>
2017-04-07 14:25:51 +0530

    blog modules

---
+++ application/modules/frontend/views/blogtemplate/blogviews.php
@@ -0,0 +1,11 @@
+<div class="blog">
+    <div class="container">
+        <div class="row">
+        	<div class="col-xs-12">
+            	<h3 class="text-center"><?php echo $blog[0]['blog_title']; ?></h3>
+                <center><img src="<?php echo $blog[0]['image_url']; ?>" alt="<?php echo $blog[0]['blog_title']; ?>" title="<?php echo $blog[0]['blog_title']; ?>"></center>
+                <p><?php echo $blog[0]['blog_description']; ?></p>
+            </div>
+        </div>
+    </div>
+</div>
