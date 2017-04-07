95e2c3198114e5d0f58bab624f403f32e748fae4
ananthan1991 <ananthan@srammram.com>
2017-04-07 18:10:05 +0530

    blog modules

--- application/modules/frontend/blocks/breadcrumbs_block.php
+++ application/modules/frontend/blocks/breadcrumbs_block.php
@@ -8,15 +8,8 @@ class Breadcrumbs_Block extends Core_Block {
 	public function drawData() {
 		$data = $this->getBlockData();
 		
-		$urlslug = $this->CI->uri->segment(3);
-		$slug = $this->CI->Mydb->custom_query("SELECT  id, page_title, page_slug FROM sramcms_cms_pages WHERE id  IN (".implode(',' , $data["params"]->cms_type).")");
-		foreach($slug as $pagebreadcrums){
-			if($pagebreadcrums['page_slug']==$urlslug){
-				$data['page_breadcrums'] = $pagebreadcrums['page_title'];
-			}
-		}
+		$data['breadcrumbs_urlslug'] = $this->CI->uri->segment(3);
 		
-	
 		$this->setView("blocks/breadcrumbs");
 		
 		return $data;
