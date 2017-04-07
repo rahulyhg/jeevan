6e1653de0d1e578747ade66b1d47805eee7d347c
ananthan1991 <ananthan@srammram.com>
2017-04-07 14:25:19 +0530

    blog modules

--- application/modules/frontend/views/blocks/blog.php
+++ application/modules/frontend/views/blocks/blog.php
@@ -5,7 +5,7 @@
 			if($blog!=''){
 				foreach($blog as $blog_list){
 			?>
-            <a href="">
+            <a href="<?php echo base_url() . 'blog/' . $blog_list['blog_slug'].'/'; ?>">
 			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ashram_div">
 				<div class="col-lg-4 col-md-6 col-sm-6 col-xs-12">
 					<img src="<?php echo $blog_list['blog_image']; ?>" alt="<?php echo $blog_list['blog_title']; ?>" title="<?php echo $blog_list['blog_title']; ?>">
