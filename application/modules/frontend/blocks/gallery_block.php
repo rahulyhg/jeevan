043241e57510d0a2fc644891c23cf1e82fcdf51a
ananthan1991 <ananthan@srammram.com>
2017-04-10 18:53:22 +0530

    gallery style change

--- application/modules/frontend/blocks/gallery_block.php
+++ application/modules/frontend/blocks/gallery_block.php
@@ -9,7 +9,7 @@ class Gallery_Block extends Core_Block {
 		$data = $this->getBlockData();
 		$category_path = media_url();
 		
-		$data['gallery'] = $this->CI->Mydb->get_all_records('id, name, slug, CONCAT("'.$category_path.'", category_image) AS category_image', 'sramcms_gallary_categories', array('is_active' => '1', 'is_delete' => '0'));
+		$data['gallery'] = $this->CI->Mydb->get_all_records('id, name, description, slug, CONCAT("'.$category_path.'", category_image) AS category_image', 'sramcms_gallary_categories', array('is_active' => '1', 'is_delete' => '0'));
 		
 		return $data;
 	}
