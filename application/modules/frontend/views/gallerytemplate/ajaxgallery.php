e558143ed3a62fd51d89b7cb176d1c0d97683b7d
ananthan1991 <ananthan@srammram.com>
2017-04-12 17:39:34 +0530

    Gallery style changes

---
+++ application/modules/frontend/views/gallerytemplate/ajaxgallery.php
@@ -0,0 +1,110 @@
+<div class="tab-content">
+    <div class="tab-pane active" id="tab1" role="tabpanel">
+        <?php
+        $i = 0;
+        $j = 2;
+        foreach ($gallery as $gallery_details) {
+        ?>
+        <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 gallery_box" data-tag=<?php
+        if ($gallery_details['media_type'] != '1') {
+            echo 'media_video';
+            
+        } else {
+            echo 'media_image';
+        }
+        ?>>
+            
+            <div class=" grid view view-first" >
+                <img src="<?php echo $gallery_details['image_url']; ?>" class="gallery_image img-responsive"/>
+               <div class="mask">
+                    <?php
+                    if ($gallery_details['media_type'] == '3') {
+                        $file_url = $gallery_details['file_name'];
+                       
+                    } elseif ($gallery_details['media_type'] == '2') {
+                        $file_url = media_url() . $gallery_details['file_name'];
+                      
+                    }else{
+                        $file_url = $gallery_details['image_url'];
+                    }
+                    ?>
+                    
+                    <center><a data-fancybox="gallery" data-width="2048" data-height="1365" data-caption="<?php echo $gallery_details['description'] ? $gallery_details['description'] :$gallery_details['title']; ?>"  title="<?php echo $gallery_details['title']; ?>" href="<?php echo $file_url; ?>" data-title="<?php echo $gallery_details['media_type']; ?>"  class="info icon_details">
+                    
+                    
+                    View</a></center>
+                </div> 
+            </div>
+            <div class="clearfix"></div>
+            <p><?php echo substr($gallery_details['description'], 0, 100); ?></p>
+        </div>
+        <?php
+            $i++;
+            if ($i % 8 == 0) {
+                echo '</div><div class="tab-pane" id="tab' . $j . '" role="tabpanel">';
+                $j++;
+            }
+        }
+        ?>
+    </div>
+</div>
+
+
+
+<div class="col-xs-12">
+
+<?php
+if(count($gallery) > 8) :
+
+$list = $j - 1;
+$numbercount = $list + 1;
+?>
+    <ul class="pager" role="tablist">
+        <li class="previous" onclick="goTo(1);"><a href="javascript:void(0);"><span aria-hidden="true"><?php echo '<<< &nbsp;&nbsp;'; ?></span>  Previous</a></li>
+        <?php
+        for ($k = 1; $k <= $list; $k++) {
+            ?>
+            <li>
+                <a aria-controls="tab<?php echo $k; ?>" data-toggle="tab" href="#tab<?php echo $k; ?>" role="tab"><?php echo $k; ?></a>
+            </li>
+            <?php
+        }
+        ?>
+        <li class="next" onclick="goTo(2);"><a href="javascript:void(0);">Next <span aria-hidden="true"><?php echo '&nbsp;&nbsp; >>>'; ?></span></a></li>
+    </ul>
+    <?php endif; ?>
+</div>
+
+<script>
+
+var list = "<?php echo $list; ?>";
+var numbercount = "<?php echo $numbercount; ?>";
+
+$('.galleryviews ul.pager li:nth-of-type(2)').addClass('active');
+
+function goTo(number) {
+
+$('.galleryviews ul.pager li:eq(' + number + ') a').tab('show');
+upgradePreNext(number);
+}
+function upgradePreNext(number) {
+
+if (number > 1) {
+$('.galleryviews ul.pager li:eq(0)').attr("onclick", "goTo(" + (number - 1) + ")");
+$('.galleryviews ul.pager li:eq(0)').attr("class", "previous");
+} else {
+$('.galleryviews ul.pager li:eq(0)').attr("class", "disabled");
+}
+if (number < list) {
+$('.galleryviews ul.pager li:eq('+ numbercount +')').attr("onclick", "goTo(" + (number + 1) + ")");
+$('.galleryviews ul.pager li:eq('+ numbercount +')').attr("class", "next");
+} else {
+$('.galleryviews ul.pager li:eq('+ numbercount +')').attr("class", "disabled");
+}
+}
+$(document).ready(function () {
+$('.galleryviews li a').on('click', function (e) {
+goTo((e.target.innerHTML) - 0);
+});
+});
+</script>
--- application/modules/frontend/views/gallerytemplate/galleryviews.php
+++ application/modules/frontend/views/gallerytemplate/galleryviews.php
@@ -3,30 +3,23 @@
         <div class="row">
 
             <h3 class="text-center"><em><?php echo $gallery[0]['gallery_category']; ?></em></h3>
-
-
-            <div class="col-xs-12 text-center">
-                
-                <button class="btn btn-small btn-warning" data-toggle="portfilter" data-target="media_image">
-                    Images
-                </button>
-                <button class="btn btn-small btn-warning" data-toggle="portfilter" data-target="media_video">
-                    Videos
-                </button>
-                <button class="btn btn-small btn-warning" data-toggle="portfilter" data-target="all">
-                    All
-                </button>
-
+			<div class="col-xs-12 text-center">
+                <a href="javascript:void(0);" class="btn btn-small btn-warning filter_gallery" id="all">All</a>
+                <a href="javascript:void(0);" class="btn btn-small btn-warning filter_gallery" id="media_image">Images</a>                    
+                <a href="javascript:void(0);" class="btn btn-small btn-warning filter_gallery" id="media_video">Videos</a>
+				<br><br>
             </div>
-            <div class="clearfix"></div>
-            <br>
-			
-            <?php
-            if (!empty($gallery)) {
-                foreach ($gallery as $gallery_details) {
+            
+            <div id="ajaxgallery">
+            
+            <div class="tab-content">
+            	<div class="tab-pane active" id="tab1" role="tabpanel">
+                	<?php
+                    $i = 0;
+                    $j = 2;
+                    foreach ($gallery as $gallery_details) {
                     ?>
-                    
-                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12" data-tag=<?php
+                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 gallery_box" data-tag=<?php
                     if ($gallery_details['media_type'] != '1') {
                         echo 'media_video';
 						
@@ -35,8 +28,8 @@
                     }
                     ?>>
 						
-                        <div class=" grid view view-sixth" >
-                            <img src="<?php echo $gallery_details['image_url']; ?>" style="width:100%;height:200px" class="gallery_image img-responsive"/>
+                        <div class=" grid view view-first" >
+                            <img src="<?php echo $gallery_details['image_url']; ?>" class="gallery_image img-responsive"/>
                            <div class="mask">
                                 <?php
                                 if ($gallery_details['media_type'] == '3') {
@@ -49,39 +42,89 @@
                                 	$file_url = $gallery_details['image_url'];
                                 }
                                 ?>
-                                <h2>Jeevanacharya</h2>
-                                <p><?php echo $gallery_details['title']; ?></p>
-                                <center><a data-fancybox="gallery" data-width="2048" data-height="1365" data-caption="<?php echo $gallery_details['description'] ? $gallery_details['description'] :$gallery_details['title']; ?>"  title="<?php echo $gallery_details['title']; ?>" href="<?php echo $file_url; ?>" data-title="<?php echo $gallery_details['media_type']; ?>"  class="info gallery_box">View</a></center>
+                                
+                                <center><a data-fancybox="gallery" data-width="2048" data-height="1365" data-caption="<?php echo $gallery_details['description'] ? $gallery_details['description'] :$gallery_details['title']; ?>"  title="<?php echo $gallery_details['title']; ?>" href="<?php echo $file_url; ?>" data-title="<?php echo $gallery_details['media_type']; ?>"  class="info icon_details">
+                                
+                                
+                                View</a></center>
                             </div> 
                         </div>
-    
+    					<div class="clearfix"></div>
+                        <p><?php echo substr($gallery_details['description'], 0, 100); ?></p>
                     </div>
-
-
                     <?php
-                }
-            }
-            ?>
-
-
-
-            <div tabindex="-1" class="modal fade" id="myModal" role="dialog">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <button class="close closemodal" type="button" data-dismiss="modal">×</button>
-                            <h3 class="modal-title">Heading</h3>
-                        </div>
-                        <div class="modal-body">
-
-                        </div>
-                        <div class="modal-footer">
-                            <button class="closemodal btn btn-default" data-dismiss="modal">Close</button>
-                        </div>
-                    </div>
+                        $i++;
+                        if ($i % 8 == 0) {
+                            echo '</div><div class="tab-pane" id="tab' . $j . '" role="tabpanel">';
+                            $j++;
+                        }
+                    }
+                    ?>
                 </div>
             </div>
+            
+            
+            
+            <div class="col-xs-12">
+            
+            <?php
+            if(count($gallery) > 8) :
+            
+            $list = $j - 1;
+            $numbercount = $list + 1;
+            ?>
+                <ul class="pager" role="tablist">
+                    <li class="previous" onclick="goTo(1);"><a href="javascript:void(0);"><span aria-hidden="true"><?php echo '<<< &nbsp;&nbsp;'; ?></span>  Previous</a></li>
+                    <?php
+                    for ($k = 1; $k <= $list; $k++) {
+                        ?>
+                        <li>
+                            <a aria-controls="tab<?php echo $k; ?>" data-toggle="tab" href="#tab<?php echo $k; ?>" role="tab"><?php echo $k; ?></a>
+                        </li>
+                        <?php
+                    }
+                    ?>
+                    <li class="next" onclick="goTo(2);"><a href="javascript:void(0);">Next <span aria-hidden="true"><?php echo '&nbsp;&nbsp; >>>'; ?></span></a></li>
+                </ul>
+                <?php endif; ?>
+            </div>
+            
+            <script>
 
+	var list = "<?php echo $list; ?>";
+	var numbercount = "<?php echo $numbercount; ?>";
+
+    $('.galleryviews ul.pager li:nth-of-type(2)').addClass('active');
+
+    function goTo(number) {
+
+        $('.galleryviews ul.pager li:eq(' + number + ') a').tab('show');
+        upgradePreNext(number);
+    }
+    function upgradePreNext(number) {
+
+        if (number > 1) {
+            $('.galleryviews ul.pager li:eq(0)').attr("onclick", "goTo(" + (number - 1) + ")");
+            $('.galleryviews ul.pager li:eq(0)').attr("class", "previous");
+        } else {
+            $('.galleryviews ul.pager li:eq(0)').attr("class", "disabled");
+        }
+        if (number < list) {
+            $('.galleryviews ul.pager li:eq('+ numbercount +')').attr("onclick", "goTo(" + (number + 1) + ")");
+            $('.galleryviews ul.pager li:eq('+ numbercount +')').attr("class", "next");
+        } else {
+            $('.galleryviews ul.pager li:eq('+ numbercount +')').attr("class", "disabled");
+        }
+    }
+    $(document).ready(function () {
+        $('.galleryviews li a').on('click', function (e) {
+            goTo((e.target.innerHTML) - 0);
+        });
+    });
+</script>
+
+			</div>
+            
         </div>
     </div>
 </div>
@@ -99,59 +142,30 @@ if(!empty($blocks['content_newsletter'])){
 echo $blocks['content_newsletter']; 
 }
 ?>
-<script src="<?php echo skin_url(); ?>js/bootstrap-portfilter.min.js"></script>
+
 <script>
-    $(document).ready(function () {
-       /* $('.gallery_box').click(function () {
+$('.filter_gallery:first-child').addClass('active');
+$('.filter_gallery').click(function(e) {
+	var slug = "<?php echo $gallery_slug; ?>";
+	var type_name = $(this).attr('id');
+	$('.filter_gallery').removeClass('active');
+	$(this).addClass('active');
+	$.ajax({
+		type: "POST",
+		dataType: "html",
+		url: admin_url +"gallery/ajaxgallery/"+slug,
+		data: {
+		   type_name:type_name,
+	  	},
+		success: function (data) {
+			$('#ajaxgallery').html(data);
+		}
+  });
+	
+});
 
-            $('.modal-body').empty();
-            var title = $(this).attr("title");
-            var media_tyle = $(this).attr("data-title");
-            $('.modal-title').html(title);
-            $($(this).parent().parent('div').html()).appendTo('.modal-body');
-            if (media_tyle == '1') {
-                $('.modal-body .video_2').hide();
-                $('.modal-body .video_1').hide();
-                $('.modal-body .gallery_image').show();
-
-
-            } else if (media_tyle == '2') {
-                $('.modal-body .gallery_image').hide();
-                $('.modal-body .video_1').hide();
-                $('.modal-body .video_2').show();
-
-                $('.modal-body .video_2').prop("autoplay", true);
-
-
-
-            } else if (media_tyle == '3') {
-                $('.modal-body .gallery_image').hide();
-                $('.modal-body .video_2').hide();
-                $('.modal-body .video_1').show();
-
-                var videoURL = $('.modal-body .video_1').prop('src');
-                videoURL += "&autoplay=1";
-                $('.modal-body .video_1').prop('src', videoURL);
-            }
-
-
-
-
-
-            $('.modal-body .overlay').hide();
-            $('.modal-body p').show();
-            $('#myModal').modal({show: true});
-            $('.gallery_box').hide();
-        });
-        $('.closemodal').click(function () {
-            $('.gallery_box').show();
-            $(".modal-body video").remove();
-            $(".modal-body iframe").remove();
-            $('.modal-body .gallery_image').hide();
-            $('.modal-body .video_1').hide();
-            $('.modal-body .video_2').hide();
-
-            $('#myModal').modal('hide');
-        });*/
-    });
 </script>
+
+
+<!--<script src="<?php echo skin_url(); ?>js/bootstrap-portfilter.min.js"></script>-->
+
