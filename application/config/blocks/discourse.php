0a66d9577d56b8d887e57ef70920bb8499bf0cb3
srammram-subhash <bose@srammram.com>
2017-04-18 17:52:08 +0530

    Added the Discourse module

---
+++ application/config/blocks/discourse.php
@@ -0,0 +1,7 @@
+<?php
+$type_params['discourse_view'] = array(
+	' ' => 'Select discourse View',
+	'discourse_one' => 'discourse One',
+	'discourse_two' => 'discourse Two',
+	'discourse_three' => 'discourse Three',
+);
