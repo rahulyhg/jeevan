17dfb934edb0bf61155514a6d294b907660d12ac
kalaivanan-sk <kannan.kalai58@gmail.com>
2017-04-22 12:31:38 +0530

    Multiple Images for the photo of the day...

--- application/modules/frontend/views/blocks/photooftheday.php
+++ application/modules/frontend/views/blocks/photooftheday.php
@@ -1,11 +1,24 @@
 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 message_of_day">
-    <h2><em><?php echo $title; ?></em></h2>
-    <?php
+ <h2><em><?php echo $title; ?></em></h2>
+<div id="owl-demo" class="owl_demo">
+   
+   <?php
 	$imagefiles =  json_decode($record_photo[0]['image']);
-	if(!empty($imagefiles->files[0])){
+		if(!empty($imagefiles->files[0])){
+		foreach($imagefiles->files as $images):
 	?>
-    <a data-fancybox="gallery" href="<?php echo media_url().$imagefiles->files[0]; ?>"> <img src="<?php echo media_url().$imagefiles->files[0]; ?>" class="img-thumbnail"  alt="jeevanacharya photo"> </a>
+		<div class="item ">
+                	<div class="col-xs-12 businesstable-content">
+                    	<div class="panel panel-pricing hvr-float-shadow">
+                        	<div class="panel-heading">
+    <a data-fancybox="gallery" href="<?php echo media_url().$images; ?>"> <img src="<?php echo media_url().$images; ?>" class="img-thumbnail"  alt="jeevanacharya photo"> </a>
+	</div>
+                   	</div>
+                   	</div>
+                </div>
     <?php
+	endforeach;
 	}
 	?>
-</div>
+		</div>
+	</div>
