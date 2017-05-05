c50760456fe697298723d338d958c2cfe4775c63
kalaivanan-sk <kannan.kalai58@gmail.com>
2017-05-05 14:20:47 +0530

    Modified Files Comitted

---
+++ application/modules/frontend/views/homepage/livetvplayer.php
@@ -0,0 +1,21 @@
+<script type="text/javascript" src="//player.wowza.com/player/latest/wowzaplayer.min.js"></script>
+<div class="clear" style="clear: both;height:2em"></div>
+<div id="playerElement" style="width:60%; height:500px;margin-left:20%"></div>
+<div class="clear" style="clear: both;height:2em"></div>
+<script type="text/javascript">
+    WowzaPlayer.create('playerElement',
+            {
+                "license": "PLAY1-8rTFG-uhfhZ-jE3AM-UGJ9r-RVTkA",
+                "title": "",
+                "description": "",
+                "sourceURL": "http%3A%2F%2F35.154.177.2%3A1935%2Flive%2FmyStream%2Fplaylist.m3u8%3FDVR",
+                "autoPlay": false,
+                "volume": "75",
+                "mute": false,
+                "loop": false,
+                "audioOnly": false,
+                "uiShowQuickRewind": true,
+                "uiQuickRewindSeconds": "30"
+            }
+    );
+</script>
