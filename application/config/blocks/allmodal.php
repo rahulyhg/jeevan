88a3c2a17d1a3c337a893cc0449fa09261f71422
ananthan1991 <ananthan@srammram.com>
2017-06-03 14:31:52 +0530

    jeevan user model

---
+++ application/config/blocks/allmodal.php
@@ -0,0 +1,8 @@
+<?php
+$type_params['modal_view'] = array(
+	' ' => 'Select Modal Type',
+	'login_modal' => 'Login Modal',
+	'register_modal' => 'Register Modal',
+	'forgot_modal' => 'Forgot Modal',
+	'resetpassword_modal' => 'Resetpassword Modal'
+);
