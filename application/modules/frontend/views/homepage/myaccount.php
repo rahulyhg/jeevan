a78496cdb4c20db344db6e40bc041a4cb0b4d52a
ananthan1991 <ananthan@srammram.com>
2017-06-05 11:43:25 +0530

    jeevan user model

--- application/modules/frontend/views/homepage/myaccount.php
+++ application/modules/frontend/views/homepage/myaccount.php
@@ -1,17 +1,22 @@
 <div class="aboutus_section">
 	<div class="container">
 		<div class="row">
-        	<?php
-			if(!empty($emailmsg)){
-			?>
-			<div class="col-xs-12 guru_aboutus text-center">
-				<h2>Account Activation</h2>
-				<p><?php echo $emailmsg; ?>.</p>
+        	
+			<div class="col-xs-12 guru_aboutus">
+            
+				<div class="col-lg-3">
+                	<img src="<?php echo $user[0]['profile']; ?>" alt="" width="280" height="280" class="">
+                </div>
+                <div class="col-lg-9">
+                	<div class="col-xs-12"><i class="fa fa-user"></i> <?php echo $user[0]['admin_username']; ?></div>
+                    <div class="col-xs-12"><i class="fa fa-user"></i> <?php echo $user[0]['admin_firstname'].' '.$user[0]['admin_lastname']; ?></div>
+                    <div class="col-xs-12"><i class="fa fa-user"></i> <?php echo $user[0]['admin_email_address']; ?></div>
+                    <div class="col-xs-12"><i class="fa fa-user"></i> <?php echo $user[0]['admin_phone_number']; ?></div>
+                    <div class="col-xs-12"><i class="fa fa-user"></i> <?php echo $user[0]['admin_country']; ?></div>
+                </div>
                 
 			</div>
-			<?php
-			}
-			?>
+			
 		</div>
 	</div>
 </div>
