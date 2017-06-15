64dfe6a1cfe8032dec8ab1e03dcaf7fbf544768a
srammram-subhash <bose@srammram.com>
2017-06-15 12:14:49 +0530

    Added the fils

--- application/modules/frontend/helpers/site_helper.php
+++ application/modules/frontend/helpers/site_helper.php
@@ -9,7 +9,7 @@ Description		:  this file contains adminpanel helper function.
 if (! function_exists ( 'get_admin_id' )) {
 	function get_admin_id() {
 		$CI = & get_instance ();
-		return  $CI->session->userdata('nc_admin_id');
+		return  $CI->session->userdata('current_user_id');
 	}
 }
 
@@ -52,6 +52,66 @@ if(!function_exists('get_client_category'))
 		return  form_dropdown('client_cate',$data,$selected,$extra);
 	}
 }
-
+/* Get Child menu */
+if (!function_exists('get_menu_childs')) {
+	function get_menu_childs($parent_id)
+	{
+		$CI = & get_instance();
+		$CI->db->select('id, name, page_id, url, parent_id, link_type, is_parent,created_on, is_active, position, target');
+		$CI->db->from('menus');
+		$CI->db->where('is_delete !=', 1);
+		if(!empty($parent_id)){
+			$CI->db->where('parent_id = ', $parent_id);
+		}
+		$has_subcats = FALSE;
+		
+		$html_out  = '';
+		$class_parent = "dropdown";
+		$html_out .= "\t\t\t\t\t".'<ul class="submenu">'."\n";
+		if ($query = $CI->db->get()) {
+			foreach ($query->result() as $row)
+			{
+				$id = $row->id;
+				$title = $row->name;
+				$link_type = $row->link_type;
+				$page_id = $row->page_id;
+				$url = $row->url;
+				$position = $row->position;
+				$target = $row->target;
+				$parent_id = $row->parent_id;
+				$is_parent = $row->is_parent;
+				
+				$has_subcats = TRUE;
+				if($link_type == 'categories'){
+					$url = frontend_url('newslist/'.get_news_category_slug($page_id));
+				}elseif($link_type == 'page'){
+					$url = frontend_url('pages/'.get_cms_page_slug($page_id));
+				}else{
+					$url = $menu_item['url'] ? $menu_item['url'] :'#';
+				}
+				if ($is_parent == TRUE)
+				{
+					
+					$html_out .= "\t\t\t\t\t\t".'<li class="dropdown">'.anchor($url, $title, 'name="'.$title.'" id="'.$id.'" target="'.$target.'"');
+					
+				}
+				else
+				{
+					$html_out .= "\t\t\t\t\t\t".'<li class="">'.anchor($url, $title, 'name="'.$title.'" id="'.$id.'" target="'.$target.'"');
+				}
+				
+				// Recurse call to get more child submenus.
+				$html_out .= get_menu_childs($id);
+			}
+			$html_out .= '</li>' . "\n";
+			$html_out .= "\t\t\t\t\t".'</ul>' . "\n";
+			
+			
+			return ($has_subcats) ? $html_out : FALSE;
+		}
+		
+		
+	}
+}
 
 
