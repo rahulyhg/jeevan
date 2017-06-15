7ba24ab8ceb64f1ccf2095859e779a90edc255db
srammram-subhash <bose@srammram.com>
2017-06-15 12:07:55 +0530

    Added the cmment line

--- application/helpers/smstemplate_helper.php
+++ application/helpers/smstemplate_helper.php
@@ -3,7 +3,7 @@
 if (! defined ( 'BASEPATH' ))
 	exit ( 'No direct script access allowed' );
 /**************************
-Project Name	: CarBuysg
+Project Name	: Jeevanacharya
 Created on		: Feb 2, 2016
 Last Modified 	: Feb 2, 2016
 Description		: Page contains Email related functions 
@@ -14,14 +14,13 @@ if(!function_exists('send_sms')){
 	function send_sms($sms_template_slug, $sms_chk_arr, $sms_rep_arr, $sms_phone, $sms_country_code){
 		
 		$CI=& get_instance();
-		$template_table = "sms_templates";
-		$setting_table  = "site_settings";
-		 $query = "SELECT sms_content , sms_variables FROM  $template_table WHERE slug = '".$sms_template_slug."'";
-         $result = $CI->Mydb->custom_query_single($query);
-		 $sms_content = $result['sms_content'];
+		$template_table = "sramcms_sms_templates";
+		$query = "SELECT sms_content , sms_variables FROM  $template_table WHERE slug = '".$sms_template_slug."'";
+		$result = $CI->Mydb->custom_query_single($query);
+		$sms_content = $result['sms_content'];
 		
 		 if(!empty($result)){
-			if($sms_country_code=='109'){
+			if($sms_country_code == '109'){
 				$sms_feedid = '362094';
 			}else{
 				$sms_feedid = '362849';
@@ -29,11 +28,12 @@ if(!function_exists('send_sms')){
 			$sms_message = str_replace($sms_chk_arr, $sms_rep_arr, $sms_content);
 			$sms_msg = str_replace(" ", "%20", $sms_message);
 			$gatewayURL = "http://bulkpush.mytoday.com/BulkSms/SingleMsgApi?feedid=".$sms_feedid."&username=8825812340&password=aadaa&To=".$sms_phone."&Text=".$sms_msg. "&senderid=''";
+			
 			$sms_ch = curl_init();
 			curl_setopt($sms_ch, CURLOPT_RETURNTRANSFER, 1);
 			curl_setopt($sms_ch, CURLOPT_URL, $gatewayURL);
 			$sms_data = curl_exec($sms_ch);
-			curl_close($ch);
+			curl_close($sms_ch);
 			$sms_xml = simplexml_load_string($sms_data);
 			$sms_json = json_encode($sms_xml);
 			
