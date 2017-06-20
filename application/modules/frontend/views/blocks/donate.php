ffac2f0ecff80b3a5607174fd0a5fb31303ba679
umamaheshwaransram <uma@srammram.com>
2017-06-20 16:55:26 +0530

    donate page created

---
+++ application/modules/frontend/views/blocks/donate.php
@@ -0,0 +1,127 @@
+<div class="donate_form">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-offset-2 col-lg-8">
+				<h3><em>Jeevancharya - 1008 Shiva Lingam Fund</em></h3>
+				<form id="contact_form" class="contactus_form row" name="contact_form" method="post" >
+					
+					<div class="donation_amount">
+						<div class="form-group col-lg-5 col-md-5 col-sm-6 col-xs-12">
+							<label for="firstname" class="form-control-label">Donation Amount </label>
+						</div>
+						<div class="form-group col-lg-7 col-md-7 col-sm-6 col-xs-12">
+							<input type="text" class="form-control" name="firstname" id="firstname">
+						</div>
+					</div>
+					<div class="donetion_detail">
+						<p>Jeevanacharya is registered as a Charitable Trust in India. Donations to Jeevanachrya are exempt from Income Tax under Section 80G of the Income Tax Act.</p>
+					</div>
+					
+					<div class="form_heading">
+						<h4>Personal Details</h4>
+					</div>
+					<input type="hidden" name="action" value="feedback">
+					<div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+						<label for="firstname" class="form-control-label">First Name</label>
+						<input type="text" class="form-control" name="firstname" id="firstname">
+					</div>
+
+					<div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+						<label for="lastname" class="form-control-label">Last Name</label>
+						<input type="text" class="form-control" name="lastname" id="lastname">
+					</div>
+					
+					<div class="form_heading">
+						<h4>Contact Details</h4>
+					</div>
+
+					<div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+						<label for="address" class="form-control-label">Address</label>
+						<input type="address" class="form-control" name="address" id="address">
+					</div>
+
+					<div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+						<label for="home_phone" class="form-control-label">Home Phone</label>
+						<input type="text" class="form-control" name="home_phone" id="home_phone">
+					</div>
+					
+					<div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+						<label for="city" class="form-control-label">City</label>
+						<input type="text" class="form-control" name="city" id="city">
+					</div>
+					
+					<div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+						<label for="work_phone" class="form-control-label">Work Phone</label>
+						<input type="text" class="form-control" name="work_phone" id="work_phone">
+					</div>
+					
+					<div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+						<label for="postal_code" class="form-control-label">Postal Code</label>
+						<input type="text" class="form-control" name="postal_code" id="postal_code">
+					</div>
+					
+					<div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+						<label for="mobile" class="form-control-label">Mobile</label>
+						<input type="text" class="form-control" name="mobile" id="mobile">
+					</div>
+					
+					<div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+						<label for="state_country" class="form-control-label">State/ Province/ County</label>
+						<input type="text" class="form-control" name="state_country" id="state_country">
+					</div>
+					
+					<div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+						<label for="phone" class="form-control-label">Email</label>
+						<input type="email" class="form-control" name="email" id="email">
+					</div>
+					
+					<div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+						<label for="country" class="form-control-label">Country</label>
+						<input type="text" class="form-control" name="country" id="country">
+					</div>
+					
+					<div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+						<label for="pan" class="form-control-label">PAN Number</label>
+						<input type="text" class="form-control" name="pan" id="pan">
+					</div>
+					
+					<div class="form_heading">
+						<h4>Contact Details</h4>
+					</div>	
+
+					<div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+						<label for="work_deatil" class="form-control-label">Work Detail</label>
+						<input type="address" class="form-control" name="work_deatil" id="work_deatil">
+					</div>
+
+					<div class="form-group col-lg-6 col-md-6 col-sm-6 col-xs-12">
+						<label for="designation" class="form-control-label">Designation</label>
+						<input type="text" class="form-control" name="designation" id="designation">
+					</div>
+					
+					<div class="form-group col-lg-12 col-md-12 col-sm-12 col-xs-12">
+						<label for="location" class="form-control-label">Location</label>
+						<input type="text" class="form-control" name="location" id="location">
+					</div>
+
+					<!--<div class="form-group col-xs-12">
+						<label for="message-text" class="form-control-label">Message</label>
+						<textarea class="form-control" name="message_text" id="message-text" rows="5"></textarea>
+					</div>-->
+					
+					<div class="checkbox">
+						<label>
+							<input type="checkbox" value="PDF" name="pdfcheckbox" id="pdfcheckbox" onchange="valueChanged()">
+							<span class="cr"><i class="cr-icon fa fa-check"></i></span>
+							 I'm contributing this amount towards the Jeevancharya - 1008 Shiva Lingam Fund 
+						</label>
+            		</div>
+					
+					<div class="form-group col-xs-12">
+						<input type="submit" class="btn btn-default" name="contact_submit" id="contact_submit" value="Continue to Payment Gateway">
+					</div>
+    			</form>
+			</div>
+		</div>
+	</div>
+</div>
